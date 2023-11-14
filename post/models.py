5a1264f285a381984767dee27a9fa91175da79a4
sarvinozsaidova <134921413+sarvinozsaidova@users.noreply.github.com>
2023-11-14 19:00:24 +0300

    first commit

--- post/models.py
+++ post/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=200)
     yaratilgan_sana = models.DateField(default=datetime.date.today)
+    image = models.ImageField(upload_to='post/static/images/', blank=True)
 
     def __str__(self) -> str:
         return self.title
